f409d0509abf9e49f6e9d9253a6222da8e380c41
unknown <CLAOFRATY@global.scd.scania.com>
2018-05-25 15:52:48 +0200

    asdf

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 const hapi = require('hapi'); 
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://ofriberg:ObcF0056wew@ds119345.mlab.com:19345/ofribergdb');
+mongoose.connection.once('open', () => {
+    console.log('connected to db');
+});
+
 // create a new instance of a hapi server
 const server = hapi.server({
     port: 4000,
     host: 'localhost'
 });
+
+
 
 const init = async () => {
 
