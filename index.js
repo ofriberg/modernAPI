90a42d428bd1a3031c107cc3eb2cc51d9f26547f
Oscar <oscfriberg@gmail.com>
2018-05-28 08:56:19 +0200

    add genom postman, npm start för att sätta igång

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // require hapi dependency
-const hapi = require('hapi'); 
+const hapi = require('hapi');
 const mongoose = require('mongoose');
+
+const Painting = require('./models/Painting');
 
 mongoose.connect('mongodb://ofriberg:ObcF0056wew@ds119345.mlab.com:19345/ofribergdb');
 mongoose.connection.once('open', () => {
@@ -15,18 +17,39 @@ const server = hapi.server({
 
 
 
-const init = async () => {
+const init = async() => {
 
-    server.route({
-        method: 'GET', // what's the method
-        path: '/', // what's the path?
-        handler: function(request, reply) { // what will happen if that path is reached?
-            return `<h1>modern api test</h1>`
-        }
-    });
+    server.route([{
+            method: 'GET', // what's the method
+            path: '/', // what's the path?
+            handler: function(request, reply) { // what will happen if that path is reached?
+                return `<h1>modern api test</h1>`
+            }
+        },
+        {
+            method: 'GET', // what's the method
+            path: '/api/v1/paintings', // what's the path?
+            handler: (request, reply) => { // what will happen if that path is reached?
+                return Painting.find();
+            }
+        },
+        {
+            method: 'POST', // what's the method
+            path: '/api/v1/paintings', // what's the path?
+            handler: (request, reply) => { // what will happen if that path is reached?
+                const { name, url, techniques } = request.payload;
+                const painting = new Painting({
+                    name,
+                    url,
+                    techniques
+                });
+                return painting.save();
+            }
+        },
+
+    ]);
     await server.start();
     console.log(`Server running at ${server.info.uri}`);
 };
 
 init();
- 
---
+++ models/Painting.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+/* 
+We declare our PaintingSchema by calling the mongoose schema constructor and passing in the options. Notice how it’s strongly typed, for example the name field can consist of a string, techniques consists of array of strings.
+*/
+
+const PaintingSchema = new Schema({
+    name: String,
+    url: String,
+    techniques: [String]
+});
+/* We export the model and name it Painting */
+module.exports = mongoose.model('Painting', PaintingSchema);
